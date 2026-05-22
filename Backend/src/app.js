e5e998f1097e53df06a103748ae0c41efb2fbb46
siddhiavhad27 <avhadsiddhi27@gmail.com>
2026-05-22 12:35:37 +0530

    Configure Backend and Frontend for Render production deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     next()
 })
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL ? [ "http://localhost:5173", process.env.FRONTEND_URL ] : "http://localhost:5173",
     credentials: true
 }))
 
